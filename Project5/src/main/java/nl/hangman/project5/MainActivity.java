933751bd3861772247daa1211693b49b21adaa30
steven <stevenvandenhout@gmail.com>
2013-05-30 14:40:53 +0200

    made beginning for Hangman class

--- Project5/src/main/java/nl/hangman/project5/MainActivity.java
+++ Project5/src/main/java/nl/hangman/project5/MainActivity.java
@@ -1,8 +1,12 @@
 package nl.hangman.project5;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+import nl.hangman.project5.models.Hangman;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // should set on screen keyboard to allways visible
+        // getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
     }
 
 
@@ -19,5 +26,16 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    // volgens mij heb ik het redelijk in de vingers deze
+    // kan ik gebruiken om een speelklasse op de achtergrond te maken
+    public void showState(View view) {
+        Hangman hangman = new Hangman();
+        String state = hangman.userInputStates[0];
+
+        hangman.initList();
+
+        TextView tx = (TextView) findViewById(R.id.textView);
+        tx.setText(state);
+    }
 }
